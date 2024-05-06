fc36f8c1760a9c1a3f405f5ac6816d6db7340ba8
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-06 16:17:26 +0800

    loginview

--- resources/views/Login/LoginView.blade.php
+++ resources/views/Login/LoginView.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>StudyPro | Login</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
         .divider:after,
