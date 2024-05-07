38772c2cf967d75a77157542a862a0d41e135500
danialnbl <danialnabil0208@gmail.com>
2024-05-07 23:35:45 +0800

    update latest

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ExpertController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ExpertController;
+use App\Http\Controllers\PublicationDataController;
 
 
 Route::get('/', function () {
@@ -11,15 +12,13 @@ Route::get('/', function () {
 
 Route::get('/login', [UserController::class, 'loginView'])->name('login');
 
-Route::get('/register',[UserController::class, 'newRegisterView']);
+Route::get('/register', [UserController::class, 'newRegisterView']);
 
 Route::get('/expert', [ExpertController::class, 'expertListView']);
 
 Route::get('/expertAdd', [ExpertController::class, 'addExpertView']);
 
-Route::get('/loginReset',[UserController::class, 'ResetPasswordView']);
-
-Route::get('/expert', [ExpertController::class, 'expertListView']);
+Route::get('/loginReset', [UserController::class, 'ResetPasswordView']);
 
 Route::get('/publication', [PublicationDataController::class, 'addPublicationDataView']);
 
