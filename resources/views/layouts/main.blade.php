4d8e5383fdd97eedf63b48f5f090988971aa4145
danialnbl <danialnabil0208@gmail.com>
2024-05-11 01:41:46 +0800

    update expert

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,12 +9,12 @@
     <link rel="icon" type="image/x-icon" href="{{ url('assets/StudyProLogo.png') }}" />
     {{-- styles --}}
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="stylesheet" href="{{ url('resources/css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.6/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/css/responsive.bootstrap5.min.css">
     <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/stepper.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -34,6 +34,7 @@
 {{-- Scripts --}}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/stepper.js') }}"></script>
 
 {{-- DataTable --}}
 <script src="https://cdn.datatables.net/2.0.6/js/dataTables.min.js"></script>
--- resources/views/manageExpertDomain/addExpertView.blade.php
+++ resources/views/manageExpertDomain/addExpertView.blade.php
@@ -2,109 +2,80 @@
 
 @section('container')
     <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
-            <div class="row justify-content-center align-items-center h-100">
-                <div class="col-12 col-lg-9 col-xl-7">
-                    <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
-                        <div class="card-body p-4 p-md-5">
-                            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
-                            <form>
-
-                                <div class="row">
-                                    <div class="col-md-6 mb-4">
-
-                                        <div data-mdb-input-init class="form-outline">
-                                            <input type="text" id="firstName" class="form-control form-control-lg" />
-                                            <label class="form-label" for="firstName">First Name</label>
-                                        </div>
-
-                                    </div>
-                                    <div class="col-md-6 mb-4">
-
-                                        <div data-mdb-input-init class="form-outline">
-                                            <input type="text" id="lastName" class="form-control form-control-lg" />
-                                            <label class="form-label" for="lastName">Last Name</label>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-6 mb-4 d-flex align-items-center">
-
-                                        <div data-mdb-input-init class="form-outline datepicker w-100">
-                                            <input type="text" class="form-control form-control-lg" id="birthdayDate" />
-                                            <label for="birthdayDate" class="form-label">Birthday</label>
-                                        </div>
-
-                                    </div>
-                                    <div class="col-md-6 mb-4">
-
-                                        <h6 class="mb-2 pb-1">Gender: </h6>
-
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                                id="femaleGender" value="option1" checked />
-                                            <label class="form-check-label" for="femaleGender">Female</label>
-                                        </div>
-
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                                id="maleGender" value="option2" />
-                                            <label class="form-check-label" for="maleGender">Male</label>
-                                        </div>
-
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                                id="otherGender" value="option3" />
-                                            <label class="form-check-label" for="otherGender">Other</label>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-6 mb-4 pb-2">
-
-                                        <div data-mdb-input-init class="form-outline">
-                                            <input type="email" id="emailAddress" class="form-control form-control-lg" />
-                                            <label class="form-label" for="emailAddress">Email</label>
-                                        </div>
-
-                                    </div>
-                                    <div class="col-md-6 mb-4 pb-2">
-
-                                        <div data-mdb-input-init class="form-outline">
-                                            <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
-                                            <label class="form-label" for="phoneNumber">Phone Number</label>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-12">
-
-                                        <select class="select form-control-lg">
-                                            <option value="1" disabled>Choose option</option>
-                                            <option value="2">Subject 1</option>
-                                            <option value="3">Subject 2</option>
-                                            <option value="4">Subject 3</option>
-                                        </select>
-                                        <label class="form-label select-label">Choose option</label>
-
-                                    </div>
-                                </div>
-
-                                <div class="mt-4 pt-2">
-                                    <input data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
-                                        type="submit" value="Submit" />
-                                </div>
-
-                            </form>
+        <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+            <div class="card-body p-4 p-md-5">
+                <h3 class="mb-4 pb-2 pb-md-0 mb-md-5"><b>Add New Expert</b></h3>
+                <div class="stepwizard col-md-offset-3 mb-4">
+                    <div class="stepwizard-row setup-panel">
+                        <div class="stepwizard-step">
+                            <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
+                            <p>Expert Details</p>
+                        </div>
+                        <div class="stepwizard-step">
+                            <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+                            <p>Research Paper Details</p>
                         </div>
                     </div>
                 </div>
+
+                <form role="form" action="" method="post">
+                    <div class="row setup-content" id="step-1">
+                        <div class="col-xs-6 col-md-offset-3">
+                            <div class="col-md-12">
+                                <h3>Expert Details</h3>
+                                <div class="form-group mb-3">
+                                    <label class="control-label">Real Name</label>
+                                    <input maxlength="100" type="text" required="required" class="form-control"
+                                        placeholder="Enter Expert Real Name">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="control-label">University Name</label>
+                                    <input maxlength="100" type="text" required="required" class="form-control"
+                                        placeholder="Enter Expert University Name">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="control-label">Email</label>
+                                    <input maxlength="100" type="text" required="required" class="form-control"
+                                        placeholder="Enter Expert Email">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="control-label">Phone Number</label>
+                                    <input maxlength="100" type="text" required="required" class="form-control"
+                                        placeholder="Enter Expert Phone Number">
+                                </div>
+                                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row setup-content" id="step-2">
+                        <div class="col-xs-6 col-md-offset-3">
+                            <div class="col-md-12">
+                                <h3> Step 2</h3>
+                                <div class="form-group">
+                                    <label class="control-label">Company Name</label>
+                                    <input maxlength="200" type="text" required="required" class="form-control"
+                                        placeholder="Enter Company Name">
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label">Company Address</label>
+                                    <input maxlength="200" type="text" required="required" class="form-control"
+                                        placeholder="Enter Company Address">
+                                </div>
+                                <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
+                                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row setup-content" id="step-3">
+                        <div class="col-xs-6 col-md-offset-3">
+                            <div class="col-md-12">
+                                <h3> Step 3</h3>
+                                <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
+                                <button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </section>
