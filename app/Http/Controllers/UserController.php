cde09b7997ea588c518211a90307b72bf6e84043
nalianadh <156681224+nalianadh@users.noreply.github.com>
2024-05-14 00:17:14 +0800

    update alia

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,7 +96,7 @@ class UserController extends Controller
         return redirect('/register')->with('status',"Success Registration!");
     }
 
-    //Verification -login
+    //Verification
 
     public function VerifyAccountView()
     {
