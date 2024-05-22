39c18a00e8eed9880cb72886fd74956163a313f6
danialnbl <danialnabil0208@gmail.com>
2024-05-23 04:34:20 +0800

    update platinum register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -80,108 +80,95 @@ class UserController extends Controller
     {
         // Validate the incoming request data
         $validatedData = $request->validate([
-            "name" => "required",
-            "ic" => "required",
-            "gender" => "required",
-            "religion" => "required",
-            "race" => "required",
-            "citizenship" => "required",
-            "address" => "required",
-            "city" => "required",
-            "state" => "required",
-            "country" => "required",
-            "zip" => "required",
-            "phoneNum" => "required",
-            "email" => "required|email",
-            "facebook" => "required",
-            "tshirtSize" => "required",
-            "dateApply" => "required|date",
-            "program" => "required",
-            "batch" => "required|integer",
-            "status" => "required",
-            "title" => "required",
-            "eduIns" => "required",
-            "sponsorship" => "required",
-            "programFee" => "required|numeric",
-            "eduLevel" => "required",
-            "occupation" => "required",
+            "P_Name" => "required",
+            "P_IC" => "required",
+            "P_Gender" => "required",
+            "P_Religion" => "required",
+            "P_Race" => "required",
+            "P_Citizenship" => "required",
+            "P_Address" => "required",
+            "P_City" => "required",
+            "P_State" => "required",
+            "P_Country" => "required",
+            "P_Zip" => "required",
+            "P_PhoneNumber" => "required",
+            "P_Email" => "required|email",
+            "P_Facebook" => "required",
+            "P_TshirtSize" => "required",
+            "P_DateApply" => "required|date",
+            "P_Program" => "required",
+            "P_Batch" => "required|integer",
+            "P_Status" => "required",
+            "P_Title" => "required",
+            "PE_EduInstitute" => "required",
+            "PE_Sponsorship" => "required",
+            "PE_ProgramFee" => "required|numeric",
+            "PE_EduLevel" => "required",
+            "PE_Occupation" => "required",
             "referral" => "required|string",
-            "referralName" => "required_if:referral,yes",
-            "referralBatch" => "required_if:referral,yes"
+            "PR_Name" => "required_if:referral,yes",
+            "PR_Batch" => "required_if:referral,yes"
         ]);
 
-        // Using DB transaction to ensure data consistency
-        DB::beginTransaction();
-
-        try {
             // Create a new PlatinumEducation instance and populate it with data
             $userEdu = new PlatinumEducation();
-            $userEdu->PE_EduInstitute = $validatedData['eduIns'];
-            $userEdu->PE_Sponsorship = $validatedData['sponsorship'];
-            $userEdu->PE_ProgramFee = $validatedData['programFee'];
-            $userEdu->PE_EduLevel = $validatedData['eduLevel'];
-            $userEdu->PE_Occupation = $validatedData['occupation'];
+            $userEdu->PE_EduInstitute = $validatedData['PE_EduInstitute'];
+            $userEdu->PE_Sponsorship = $validatedData['PE_Sponsorship'];
+            $userEdu->PE_ProgramFee = $validatedData['PE_ProgramFee'];
+            $userEdu->PE_EduLevel = $validatedData['PE_EduLevel'];
+            $userEdu->PE_Occupation = $validatedData['PE_Occupation'];
             $userEdu->save();
 
             if ($request->referral === 'yes') {
                 $userRef = new PlatinumReferral();
-                $userRef->PR_Name = $validatedData['referralName'];
-                $userRef->PR_Batch = $validatedData['referralBatch'];
+                $userRef->PR_Name = $validatedData['PR_Name'];
+                $userRef->PR_Batch = $validatedData['PR_Batch'];
                 $userRef->save();
             }
 
             // Create a new Platinum instance and populate it with data
             $user = new Platinum();
-            $user->P_IC = $validatedData['ic'];
-            $user->P_Name = $validatedData['name'];
-            $user->P_Gender = $validatedData['gender'];
-            $user->P_Religion = $validatedData['religion'];
-            $user->P_Race = $validatedData['race'];
-            $user->P_Citizenship = $validatedData['citizenship'];
-            $user->P_Address = $validatedData['address'];
-            $user->P_City = $validatedData['city'];
-            $user->P_State = $validatedData['state'];
-            $user->P_Country = $validatedData['country'];
-            $user->P_Zip = $validatedData['zip'];
-            $user->P_PhoneNumber = $validatedData['phoneNum'];
-            $user->P_Email = $validatedData['email'];
-            $user->P_Facebook = $validatedData['facebook'];
-            $user->P_TshirtSize = $validatedData['tshirtSize'];
-            $user->P_DateApply = $validatedData['dateApply'];
-            $user->P_Program = $validatedData['program'];
-            $user->P_Batch = $validatedData['batch'];
-            $user->P_Status = $validatedData['status'];
-            $user->P_Title = $validatedData['title'];
-
-
-
+            $user->P_IC = $validatedData['P_IC'];
+            $user->P_Name = $validatedData['P_Name'];
+            $user->P_Gender = $validatedData['P_Gender'];
+            $user->P_Religion = $validatedData['P_Religion'];
+            $user->P_Race = $validatedData['P_Race'];
+            $user->P_Citizenship = $validatedData['P_Citizenship'];
+            $user->P_Address = $validatedData['P_Address'];
+            $user->P_City = $validatedData['P_City'];
+            $user->P_State = $validatedData['P_State'];
+            $user->P_Country = $validatedData['P_Country'];
+            $user->P_Zip = $validatedData['P_Zip'];
+            $user->P_PhoneNumber = $validatedData['P_PhoneNumber'];
+            $user->P_Email = $validatedData['P_Email'];
+            $user->P_Facebook = $validatedData['P_Facebook'];
+            $user->P_TshirtSize = $validatedData['P_TshirtSize'];
+            $user->P_DateApply = $validatedData['P_DateApply'];
+            $user->P_Program = $validatedData['P_Program'];
+            $user->P_Batch = $validatedData['P_Batch'];
+            $user->P_Status = $validatedData['P_Status'];
+            $user->P_Title = $validatedData['P_Title'];
             // Set the PE_Id field with the ID of the PlatinumEducation instance
             $user->PE_Id = $userEdu->id;
+            // Set the PR_Id field with the ID of the PlatinumReferal instance
+            $user->PR_Id = $userRef->id;
+
             $user->save();
 
-            // Create a new LoginAccount instance and populate it with data
-            $loginAcc = new LoginAccount();
-            $loginAcc->LA_Username = $validatedData['ic'];
-            // Hash the password before storing it
-            $loginAcc->LA_Password = password_hash($validatedData['ic'], PASSWORD_BCRYPT);
-            $loginAcc->P_IC = $validatedData['ic'];
-            $loginAcc->save();
+            $newUser = new User;
+            $newUser->name = $validatedData['P_Name'];
+            $newUser->LA_Username = $validatedData['P_Email'];
+            $newUser->email = $validatedData['P_Email'];
+            $newUser->password = Hash::make($validatedData['P_IC']);
+            $newUser->P_IC = $validatedData['P_IC'];
+            $newUser->LA_Role = 0;
 
-            // Commit the transaction
-            DB::commit();
-
-            // Redirect with success message
-            return redirect()->route("register")->with("success", "Success Registration!");
-        } catch (\Exception $e) {
-            // Rollback the transaction on failure
-            DB::rollBack();
-
-            // Log the error for debugging purposes
-            Log::error('Failed to register user: ' . $e->getMessage());
-
+            if ($newUser->save()) {
+                // Redirect with success message
+                return redirect()->route("register")->with("success", "Success Registration!");
+            }
             // Redirect with error message
-            return redirect()->route("register")->with("error", "Failed to register: " . $e->getMessage());
-        }
+            return redirect()->route("register")->with("error", "Failed to register!");
     }
 
     public function StaffRegisterPost(Request $request)
@@ -214,7 +201,8 @@ class UserController extends Controller
             if ($newUser->save()) {
                 return redirect()->route("staffReg")->with("success", "Success Registration!");
             }
-            return to_route('staffReg', ['message' => 'saving to db failed, RARE ENDING']);
+            // Redirect with error message
+            return redirect()->route("staffReg")->with("error", "Failed to register!");
         }
     public function MentorRegisterPost(Request $request)
     {
--- resources/views/manageRegistration/PlatinumRegistration.blade.php
+++ resources/views/manageRegistration/PlatinumRegistration.blade.php
@@ -14,45 +14,45 @@
         @endif
        <!-- START FORM -->
        <form action="{{ route('register.submit') }}" method='post'>
-            @csrf  
+            @csrf
             <div class="my-3 p-5 bg-body rounded shadow-sm">
                 <h1>Platinum Registration</h1>
                 <h2>Platinum details:</h2>
                 <!-- Form Fields Here -->
                 <div class="mb-3 row">
-                    <label for="name" class="col-sm-2 col-form-label">Name</label>
+                    <label for="P_Name" class="col-sm-2 col-form-label">Name</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name='name' id="name" value="{{ old('name') }}" autofocus>
+                        <input type="text" class="form-control" name='P_Name' id="P_Name" value="{{ old('P_Name') }}" autofocus>
                     </div>
                 </div>
                 <div class="mb-3 row">
-                    <label for="ic" class="col-sm-2 col-form-label">NR IC</label>
+                    <label for="P_IC" class="col-sm-2 col-form-label">NR IC</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name='ic' id="ic" value="{{ old('ic') }}">
+                        <input type="text" class="form-control" name='P_IC' id="P_IC" value="{{ old('P_IC') }}">
                     </div>
                 </div>
                 <div class="mb-3 row">
-                    <label for="gender" class="col-sm-2 col-form-label">Gender</label>
+                    <label for="P_Gender" class="col-sm-2 col-form-label">Gender</label>
                     <div class="col-sm-10">
-                        <input type="radio" name="gender" value="male" {{ old('gender') == 'male' ? 'checked' : '' }}> Male
-                        <input type="radio" name="gender" value="female" {{ old('gender') == 'female' ? 'checked' : '' }}> Female
+                        <input type="radio" name="P_Gender" value="male" {{ old('P_Gender') == 'male' ? 'checked' : '' }}> Male
+                        <input type="radio" name="P_Gender" value="female" {{ old('P_Gender') == 'female' ? 'checked' : '' }}> Female
                     </div>
                 </div>
             <div class="mb-3 row">
-                <label for="religion" class="col-sm-2 col-form-label">Religion</label>
+                <label for="P_Religion" class="col-sm-2 col-form-label">Religion</label>
                 <div class="col-sm-10">
-                <input type="radio" name="religion" value="islam"> Islam
-                <input type="radio" name="religion" value="christian"> Christian
-                <input type="radio" name="religion" value="buddha"> Buddha
+                <input type="radio" name="P_Religion" value="islam"> Islam
+                <input type="radio" name="P_Religion" value="christian"> Christian
+                <input type="radio" name="P_Religion" value="buddha"> Buddha
                 </div>
             </div>
             <div class="mb-3 row">
-            <label for="race" class="col-sm-2 col-form-label">Race</label>
+            <label for="P_Race" class="col-sm-2 col-form-label">Race</label>
             <div class="col-sm-10">
-                <input type="radio" name="race" value="malay"> Malay
-                <input type="radio" name="race" value="indian"> Indian
-                <input type="radio" name="race" value="chinese"> Chinese
-                <input type="radio" name="race" value="others" id="race_others"> Others
+                <input type="radio" name="P_Race" value="malay"> Malay
+                <input type="radio" name="P_Race" value="indian"> Indian
+                <input type="radio" name="P_Race" value="chinese"> Chinese
+                <input type="radio" name="P_Race" value="others" id="race_others"> Others
                 <input type="text" name="race_others_textbox" id="race_others_textbox" style="display:none;">
             </div>
         </div>
@@ -69,99 +69,99 @@
         </script>
 
             <div class="mb-3 row">
-                <label for="citizenship" class="col-sm-2 col-form-label">Citizenship</label>
+                <label for="P_Citizenship" class="col-sm-2 col-form-label">Citizenship</label>
                 <div class="col-sm-10">
-                <input type="radio" name="citizenship" value="malaysian"> Malaysian
-                <input type="radio" name="citizenship" value="nonMalay"> Non-Malaysian
+                <input type="radio" name="P_Citizenship" value="malaysian"> Malaysian
+                <input type="radio" name="P_Citizenship" value="nonMalay"> Non-Malaysian
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="address" class="col-sm-2 col-form-label">Address</label>
+                <label for="P_Address" class="col-sm-2 col-form-label">Address</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="address" id="address">
+                    <input type="text" class="form-control" name="P_Address" id="P_Address">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="city" class="col-sm-2 col-form-label">City</label>
+                <label for="P_City" class="col-sm-2 col-form-label">City</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="city" id="city">
+                    <input type="text" class="form-control" name="P_City" id="P_City">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="state" class="col-sm-2 col-form-label">State</label>
+                <label for="P_State" class="col-sm-2 col-form-label">State</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="state" id="state">
+                    <input type="text" class="form-control" name="P_State" id="P_State">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="country" class="col-sm-2 col-form-label">Country</label>
+                <label for="P_Country" class="col-sm-2 col-form-label">Country</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="country" id="country">
+                    <input type="text" class="form-control" name="P_Country" id="P_Country">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="zip" class="col-sm-2 col-form-label">Zip Code</label>
+                <label for="P_Zip" class="col-sm-2 col-form-label">Zip Code</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="zip" id="zip">
+                    <input type="text" class="form-control" name="P_Zip" id="P_Zip">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="phoneNum" class="col-sm-2 col-form-label">Phone Number</label>
+                <label for="P_PhoneNumber" class="col-sm-2 col-form-label">Phone Number</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="phoneNum" id="phoneNum">
+                    <input type="text" class="form-control" name="P_PhoneNumber" id="P_PhoneNumber">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <label for="P_Email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" name="email" id="email">
+                    <input type="email" class="form-control" name="P_Email" id="P_Email">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="facebook" class="col-sm-2 col-form-label">Facebook</label>
+                <label for="P_Facebook" class="col-sm-2 col-form-label">Facebook</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="facebook" id="facebook">
+                    <input type="text" class="form-control" name="P_Facebook" id="P_Facebook">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="tshirtSize" class="col-sm-2 col-form-label">T-Shirt Size</label>
+                <label for="P_TshirtSize" class="col-sm-2 col-form-label">T-Shirt Size</label>
                 <div class="col-sm-10">
-                <input type="radio" name="tshirtSize" value="xs"> XS
-                <input type="radio" name="tshirtSize" value="s"> S
-                <input type="radio" name="tshirtSize" value="m"> M
-                <input type="radio" name="tshirtSize" value="l"> L
-                <input type="radio" name="tshirtSize" value="xl"> XL
-                <input type="radio" name="tshirtSize" value="xxl"> XXL
+                <input type="radio" name="P_TshirtSize" value="xs"> XS
+                <input type="radio" name="P_TshirtSize" value="s"> S
+                <input type="radio" name="P_TshirtSize" value="m"> M
+                <input type="radio" name="P_TshirtSize" value="l"> L
+                <input type="radio" name="P_TshirtSize" value="xl"> XL
+                <input type="radio" name="P_TshirtSize" value="xxl"> XXL
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="dateApply" class="col-sm-2 col-form-label">Date Apply</label>
+                <label for="P_DateApply" class="col-sm-2 col-form-label">Date Apply</label>
                 <div class="col-sm-10">
-                    <input type="date" class="form-control" name="dateApply" id="dateApply">
+                    <input type="date" class="form-control" name="P_DateApply" id="P_DateApply">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="program" class="col-sm-2 col-form-label">Program</label>
+                <label for="P_Program" class="col-sm-2 col-form-label">Program</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="program" id="program">
+                    <input type="text" class="form-control" name="P_Program" id="P_Program">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="batch" class="col-sm-2 col-form-label">Batch</label>
+                <label for="P_Batch" class="col-sm-2 col-form-label">Batch</label>
                 <div class="col-sm-10">
-                    <input type="number" class="form-control" name="batch" id="batch">
+                    <input type="number" class="form-control" name="P_Batch" id="P_Batch">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="status" class="col-sm-2 col-form-label">Status</label>
+                <label for="P_Status" class="col-sm-2 col-form-label">Status</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="status" id="status">
+                    <input type="text" class="form-control" name="P_Status" id="P_Status">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="title" class="col-sm-2 col-form-label">Title</label>
+                <label for="P_Title" class="col-sm-2 col-form-label">Title</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="title" id="title">
+                    <input type="text" class="form-control" name="P_Title" id="P_Title">
                 </div>
             </div>
             <!-- Referral -->
@@ -174,15 +174,15 @@
                     </div>
                 </div>
                 <div class="mb-3 row referral-info" id="referralInfoName" style="display:none;">
-                    <label for="referralName" class="col-sm-2 col-form-label">Referral Name</label>
+                    <label for="PR_Name" class="col-sm-2 col-form-label">Referral Name</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="referralName" name="referralName" value="{{ old('referralName') }}">
+                        <input type="text" class="form-control" id="PR_Name" name="PR_Name" value="{{ old('PR_Name') }}">
                     </div>
                 </div>
                 <div class="mb-3 row referral-info" id="referralInfoBatch" style="display:none;">
-                    <label for="referralBatch" class="col-sm-2 col-form-label">Referral Batch</label>
+                    <label for="PR_Batch" class="col-sm-2 col-form-label">Referral Batch</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="referralBatch" name="referralBatch" value="{{ old('referralBatch') }}">
+                        <input type="text" class="form-control" id="PR_Batch" name="PR_Batch" value="{{ old('PR_Batch') }}">
                     </div>
                 </div>
 
@@ -210,42 +210,33 @@
 
             <h2>Education details: </h2>
             <div class="mb-3 row">
-                <label for="eduIns" class="col-sm-2 col-form-label">Education Institute</label>
+                <label for="PE_EduInstitute" class="col-sm-2 col-form-label">Education Institute</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="eduIns" id="eduIns">
+                    <input type="text" class="form-control" name="PE_EduInstitute" id="PE_EduInstitute">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="sponsorship" class="col-sm-2 col-form-label">Sponsorship</label>
+                <label for="PE_Sponsorship" class="col-sm-2 col-form-label">Sponsorship</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="sponsorship" id="sponsorship">
+                    <input type="text" class="form-control" name="PE_Sponsorship" id="PE_Sponsorship">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="programFee" class="col-sm-2 col-form-label">Program Fee</label>
+                <label for="PE_ProgramFee" class="col-sm-2 col-form-label">Program Fee</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="programFee" id="programFee">
+                    <input type="text" class="form-control" name="PE_ProgramFee" id="PE_ProgramFee">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="eduLevel" class="col-sm-2 col-form-label">Education Level</label>
+                <label for="PE_EduLevel" class="col-sm-2 col-form-label">Education Level</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="eduLevel" id="eduLevel">
+                    <input type="text" class="form-control" name="PE_EduLevel" id="PE_EduLevel">
                 </div>
             </div>
             <div class="mb-3 row">
-                <label for="occupation" class="col-sm-2 col-form-label">Occupation</label>
+                <label for="PE_Occupation" class="col-sm-2 col-form-label">Occupation</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" name="occupation" id="occupation">
-                </div>
-            </div>
-            <!--this one tak perlu masuk database just as condition for login-->
-            <div class="mb-3 row">
-                <label for="role" class="col-sm-2 col-form-label">Role</label>
-                <div class="col-sm-10">
-                <input type="radio" name="role" value="Staff"> Staff
-                <input type="radio" name="role" value="Platinum"> Platinum
-                <input type="radio" name="role" value="Mentor"> Mentor
+                    <input type="text" class="form-control" name="PE_Occupation" id="PE_Occupation">
                 </div>
             </div>
             <!--button-->
@@ -256,7 +247,7 @@
         </form>
         </div>
         <!-- AKHIR FORM -->
-        
+
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     @endsection
